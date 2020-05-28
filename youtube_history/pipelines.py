d46a3dcb4c03632ac46ce82ad43691f8c6a4387a
nerdvarma <sangeethsvarma@gmail.com>
2020-05-28 17:35:57 +0530

    Update pipelines.py

--- youtube_history/pipelines.py
+++ youtube_history/pipelines.py
@@ -7,6 +7,8 @@
 
 from scrapy.exceptions import DropItem
 import string
+from datetime import date, datetime, timedelta
+
 
 class ConvertDatesPipeline(object):
     def process_item(self, item, spider):
@@ -14,34 +16,24 @@ class ConvertDatesPipeline(object):
         return item
 
     def date_parsing(self, datestring):
-        # Date string is converted from MMM DD, YYYY to MM/DD/YYYY
-        # TODO: Handle the parsing for upto one week prior to scraping date which is in the format of Tuesday, Friday, etc
-        if "Jan" in datestring:
-            formatteddate = "01"
-        if "Feb" in datestring:
-            formatteddate = "02"
-        if "Mar" in datestring:
-            formatteddate = "03"
-        if "Apr" in datestring:
-            formatteddate = "04"
-        if "May" in datestring:
-            formatteddate = "05"
-        if "Jun" in datestring:
-            formatteddate = "06"
-        if "Jul" in datestring:
-            formatteddate = "07"
-        if "Aug" in datestring:
-            formatteddate = "08"
-        if "Sep" in datestring:
-            formatteddate = "09"
-        if "Oct" in datestring:
-            formatteddate = "10"
-        if "Nov" in datestring:
-            formatteddate = "11"
-        if "Dec" in datestring:
-            formatteddate = "12"
-        formatteddate = formatteddate + "/" + "/".join(datestring[4:].split(", "))
-        return formatteddate
+        today = date.today()
+        weekdays = {
+            (today - timedelta(i)).strftime("%A"): today - timedelta(i)
+            for i in range(2, 7)
+        }
+        weekdays["Today"] = today
+        weekdays["Yesterday"] = today - timedelta(1)
+
+        re1 = re.compile(
+            r"([JFMASOND][aepuco][nbrylgptvc]) (\d{1,2})(, 2\d{3})*")
+        if datestring in weekdays.keys():
+            return weekdays[datestring]
+        else:
+            m = re1.match(datestring)
+            if m.groups()[2] is None:
+                datestring += f", {today.year}"
+            return datetime.strptime(datestring, "%b %d, %Y").date()
+
 
 class CleanUpHistoryEntriesPipeline(object):
     def proccess_items(self, item, spider):
@@ -49,6 +41,7 @@ class CleanUpHistoryEntriesPipeline(object):
         # item['author_id'] = item['author_id'].replace('/user/', '')
         # item['author_id'] = item['author_id'].replace('/channel/', '')
         return item
+
 
 class ConvertVideoTimePipeline(object):
     def process_item(self, item, spider):
@@ -71,6 +64,7 @@ class ConvertVideoTimePipeline(object):
                 total_seconds += hours * 3600
         return total_seconds
 
+
 class DbOutputPipeline(object):
     def __init__(self, *args, **kwargs):
         super(DbOutputPipeline, *args, **kwargs)
@@ -78,7 +72,8 @@ class DbOutputPipeline(object):
         self.db = db_api.AppDatabase()
 
     def process_item(self, item, spider):
-        keys = ["vid","channel","channel_url","title","description","time","date"]
+        keys = ["vid", "channel", "channel_url",
+                "title", "description", "time", "date"]
         args = []
         for k in keys:
             args.append(item[k])
